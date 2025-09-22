0676e94f38746cb971a1d68d335223d4f7ec825f
Rainer Stropek <rainer@software-architects.at>
2025-09-22 10:39:35 +0200

    Add OpenAI response handling and configure diagnostics in Conversations API

--- ChatBot/Conversations.cs
+++ ChatBot/Conversations.cs
@@ -1,3 +1,5 @@
+using OpenAI.Responses;
+
 namespace ChatBot;
 
 public static class ConversationsEndpoints
@@ -9,9 +11,27 @@ public static class ConversationsEndpoints
             var api = app.MapGroup("/conversations");
 
             // Add routes here
-            api.MapGet("/", () => "Hello from Conversations API!");
+            api.MapGet("/", TryOpenAI);
 
             return app;
         }
+    }
+
+    public static async Task<IResult> TryOpenAI(IConfiguration config)
+    {
+        var client = new OpenAIResponseClient(config["OPENAI_MODEL"], config["OPENAI_API_KEY"]);
+
+        var options = new ResponseCreationOptions
+        {
+            Instructions = "Du bist der Papagei eines Piraten. Antworte so, mit vielen Emojis",
+            ReasoningOptions = new()
+            {
+                ReasoningEffortLevel = ResponseReasoningEffortLevel.Low
+            }
+        };
+
+        var response = await client.CreateResponseAsync("Sind Delphine Fische?", options);
+
+        return Results.Ok(response.Value.GetOutputText());
     }
 }
